e2bd932fc5f189f061f3b189248a419f7b6dc3d2
Denis <demaz@bk.ru>
2022-10-18 15:52:40 +0700

    first commit

--- src/main/java/pro/sky/java/course2/calculator/calculator/Service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/calculator/Service/CalculatorServiceImpl.java
@@ -23,7 +23,7 @@ public class CalculatorServiceImpl implements CalculatorService{
     @Override
     public double calculateQuotient(Integer num1, Integer num2) {
         if (num2 == 0) {
-            throw new RuntimeException("Нельзя делить на ноль");
+            throw new IllegalArgumentException("Нельзя делить на ноль");
         }
         return (double) num1 / num2;
     }
